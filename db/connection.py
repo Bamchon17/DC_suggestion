76ca6fe492b81f6282fd12de88419dbdf43f324f
TaeNontakorn <145709069+TaeNontakorn@users.noreply.github.com>
2025-08-18 12:19:40 +0700

    push code manday _calculation

--- db/connection.py
+++ db/connection.py
@@ -1,15 +1,30 @@
 import psycopg2
-import os
 from dotenv import load_dotenv
+import os
 
-load_dotenv()
 
-conn = psycopg2.connect(
-    host=os.getenv("DB_HOST"),
-    dbname=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    port=os.getenv("DB_PORT")
-)
+# โหลด .env จาก root directory
+dotenv_path = os.path.join(os.path.dirname(__file__), "..", ".env")
+load_dotenv(dotenv_path)
 
-cursor = conn.cursor()
+try:
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
+    )
+
+    cursor = conn.cursor()
+    
+    # ลองเช็ค version ของ DB
+    cursor.execute("SELECT version();")
+    db_version = cursor.fetchone()
+    print("Connected to:", db_version)
+
+    cursor.close()
+    
+
+except Exception as e:
+    print("Connection failed:", e)
